5a77aaab3062453f201cff182f41a1c2ff4275be
Nazar Kuziv <nazar123kuziv@gmail.com>
2020-10-31 10:40:53 +0200

    first commit

---
+++ Project 4.2.cpp
@@ -0,0 +1,42 @@
+﻿// Lab_04_2.cpp
+ // < Кузів Назар >
+ // Лабораторна робота № 4.2
+ // Табуляція функції, заданої формулою: функція однієї змінної
+ // Варіант 6
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = 5 * exp(3 * x);
+		if (x <= -1)
+			B = 3 + sin(abs(x));
+		else
+			if (x > 3)
+				B = 7 - pow(2 * x, 3 / 2);
+			else
+				B = 2 * exp(x / 4 - 1);
+		y = A - B;
+		cout << "|" << setw(7) << setprecision(3) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
